7e32e7290a4451edd4f7bc8b9a58c8a5d44473e3
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-03-28 19:11:25 -0300

    feat: models

---
+++ back-end/src/database/models/saleProduct.model.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, DataTypes) => {
+  const SaleProductTable = sequelize.define('SaleProduct', {
+    saleId: DataTypes.INTEGER,
+    productId: DataTypes.INTEGER,
+    quantity: DataTypes.INTEGER,
+  }, {
+    tableName: 'sales_products',
+    underscored: true,
+    timestamps: false
+  });
+
+  SaleProductTable.associate = ({ Sale, Product}) => {
+    SaleProductTable.belongsToMany(Sale, { foreignKey: 'saleId', as: 'sale'})
+    SaleProductTable.belongsToMany(Product, { foreignKey: 'productId', as: 'product'})
+  }
+
+  return SaleProductTable;
+}
