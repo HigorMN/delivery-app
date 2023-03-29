9dff099ca296a737c21cec126e04121d1054f8ae
Marcelo Vieira <marcelodefy15@gmail.com>
2023-03-29 16:38:30 -0300

    Feat - Construídos elementos da tela de login

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -5,7 +5,33 @@ export default function Login() {
   return (
     <>
       <Redirect to="/login" />
-      <div>Login</div>
+
+      <div>
+        <img src="" alt="" />
+        <h1>nome app</h1>
+        <form>
+          <label htmlFor="login">
+            Login
+            <input type="email" id="login" data-testid="common_login__input-email" />
+          </label>
+          <label htmlFor="password">
+            Senha
+            <input
+              type="password"
+              id="password"
+              data-testid="common_login__input-password"
+            />
+
+          </label>
+
+          <button type="submit" data-testid="common_login__button-login">LOGIN</button>
+          <button type="button" data-testid="common_login__button-register">
+            Ainda não tenho conta
+          </button>
+        </form>
+        <p data-testid="common_login__element-invalid-email">Mensagem de erro</p>
+      </div>
+
     </>
   );
 }
