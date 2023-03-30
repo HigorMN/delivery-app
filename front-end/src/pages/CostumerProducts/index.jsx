bd756b398b7cd63e0ecf667bb229c08c738ade2a
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-03-30 18:43:05 -0300

    feat: card de produtos

--- front-end/src/pages/CostumerProducts/index.jsx
+++ front-end/src/pages/CostumerProducts/index.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import CardProduct from '../../components/CardProduct';
 import Header from '../../components/Header';
+import api from '../../utils/api';
 
 function Costumer() {
+  const [dataProduct, setDataProduct] = useState([]);
+
+  const getProduct = async () => {
+    const { data } = await api.get('/products');
+    setDataProduct(data);
+  };
+
+  useEffect(() => {
+    getProduct();
+  });
+
   return (
     <>
       <Header />
-      <div>
-        <h1>costumer</h1>
-      </div>
+      <main>
+        { dataProduct.map((e, index) => (
+          <CardProduct
+            key={ index }
+            id={ e.id }
+            image={ e.urlImage }
+            name={ e.name }
+            price={ e.price }
+            quantity={ 0 }
+          />
+        ))}
+      </main>
     </>
   );
 }
