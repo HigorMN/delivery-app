ddf58edbe4a0c87c1ab7828d8e50a47eb62eb804
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-03-30 16:35:22 -0300

    refactor: header

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import Link from '../GenericLink';
+
+const ROUTE_CUSTOMER = 'customer_products';
+const ELEMENT_PRODUCTS = 'element-navbar-link-products';
+const ELEMENT_ORDERS = 'element-navbar-link-orders';
+const ELEMENT_USER = 'element-navbar-user-full-name';
+const ELEMENT_LOGOUT = 'element-navbar-link-logout';
 
 export default function Header() {
   return (
@@ -7,30 +13,26 @@ export default function Header() {
       <nav>
         <Link
           to="/"
-          data-testid="customer_products__element-navbar-link-products"
-        >
-          PRODUTOS
-        </Link>
+          title="PRODUTOS"
+          dataTestId={ `${ROUTE_CUSTOMER}__${ELEMENT_PRODUCTS}` }
+        />
         <Link
           to="/"
-          data-testid="customer_products__element-navbar-link-orders"
-        >
-          MEUS PEDIDOS
-        </Link>
+          title="MEUS PEDIDOS"
+          dataTestId={ `${ROUTE_CUSTOMER}__${ELEMENT_ORDERS}` }
+        />
       </nav>
       <nav>
         <Link
           to="/"
-          data-testid="customer_products__element-navbar-user-full-name"
-        >
-          Nome Completo
-        </Link>
+          title="Nome Completo"
+          dataTestId={ `${ROUTE_CUSTOMER}__${ELEMENT_USER}` }
+        />
         <Link
           to="/"
-          data-testid="customer_products__element-navbar-link-logout"
-        >
-          Sair
-        </Link>
+          title="Sair"
+          dataTestId={ `${ROUTE_CUSTOMER}__${ELEMENT_LOGOUT}` }
+        />
       </nav>
     </header>
   );
