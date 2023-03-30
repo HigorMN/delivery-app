d962d4033f5f6b95d48eb8e2363063862eba05d2
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-03-30 19:35:20 -0300

    fix: resolvendo conflito

--- front-end/src/pages/Login/index.js
+++ front-end/src/pages/Login/index.js
@@ -1,5 +1,4 @@
-
-import React, { useState, useEffect, useHistory } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Redirect, useHistory } from 'react-router-dom';
 import api from '../../utils/api';
 
