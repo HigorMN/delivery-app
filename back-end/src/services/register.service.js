ff61e1f6b0f238a9b48e676c03bf0b3c2683fafa
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-03-31 14:34:31 -0300

    feat: retorno do registro

--- back-end/src/services/register.service.js
+++ back-end/src/services/register.service.js
@@ -1,18 +1,18 @@
 const md5 = require('md5');
 const { response } = require('../utils/response');
 const { User } = require('../database/models');
+const generateJwt = require('../utils/generateJwt');
 
 const createRegister = async (body) => {
   const { name, email, password } = body;
   const hashed = md5(password);
-  await User.create({
-    name,
-    email,
-    password: hashed,
-    role: 'customer',
-  });
 
-  return response(201, '');
+  const userPayload = { name, email, role: 'customer' };
+  await User.create({ ...userPayload, password: hashed });
+  
+  const token = generateJwt(userPayload);
+
+  return response(201, { ...userPayload, token });
 };
 
 const findUser = async (key, value) => {
