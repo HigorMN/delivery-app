ee53134befb5b089d0d5acfa56f64b40e4160516
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-04-03 18:06:52 -0300

    feat: rotas de login

--- front-end/src/components/Header/index.jsx
+++ front-end/src/components/Header/index.jsx
@@ -1,46 +1,13 @@
 import React from 'react';
-import { Redirect } from 'react-router-dom';
-import Link from '../GenericLink';
-
-const ROUTE_CUSTOMER = 'customer_products';
-const ELEMENT_PRODUCTS = 'element-navbar-link-products';
-const ELEMENT_ORDERS = 'element-navbar-link-orders';
-const ELEMENT_USER = 'element-navbar-user-full-name';
-const ELEMENT_LOGOUT = 'element-navbar-link-logout';
+import { useLocation } from 'react-router-dom';
+import NavCustomer from '../NavCustomer';
 
 export default function Header() {
-  const localUser = JSON.parse(localStorage.getItem('user'));
+  const { pathname } = useLocation();
 
-  const handleLogOut = () => localStorage.removeItem('user');
-
-  if (!localUser) return <Redirect to="/login" />;
   return (
     <header>
-      <nav>
-        <Link
-          to="/"
-          title="PRODUTOS"
-          dataTestId={ `${ROUTE_CUSTOMER}__${ELEMENT_PRODUCTS}` }
-        />
-        <Link
-          to="/"
-          title="MEUS PEDIDOS"
-          dataTestId={ `${ROUTE_CUSTOMER}__${ELEMENT_ORDERS}` }
-        />
-      </nav>
-      <nav>
-        <Link
-          to="/"
-          title={ localUser ? localUser.name : '' }
-          dataTestId={ `${ROUTE_CUSTOMER}__${ELEMENT_USER}` }
-        />
-        <Link
-          to="/"
-          onClick={ handleLogOut }
-          title="Sair"
-          dataTestId={ `${ROUTE_CUSTOMER}__${ELEMENT_LOGOUT}` }
-        />
-      </nav>
+      { pathname.includes('customer') && <NavCustomer /> }
     </header>
   );
 }
