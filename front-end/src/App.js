38119d6663b5d4f0b4c8119d217729ceb289f9be
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-04-04 14:27:05 -0300

    feat: contexto de auth

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,6 +6,8 @@ import Register from './pages/Register';
 import CostumerProducts from './pages/CostumerProducts';
 import ProviderProduct from './hooks/productContext/Provider';
 import CustomerCheckout from './pages/CustomerCheckout';
+import AdminManage from './pages/AdminManage';
+import ProviderAuth from './hooks/authContext/Provider';
 
 function App() {
   return (
@@ -14,10 +16,13 @@ function App() {
         <Route exact path="/" component={ Login } />
         <Route exact path="/login" component={ Login } />
         <Route exact path="/register" component={ Register } />
-        <ProviderProduct>
-          <Route exact path="/customer/products" component={ CostumerProducts } />
-          <Route exact path="/customer/checkout" component={ CustomerCheckout } />
-        </ProviderProduct>
+        <ProviderAuth>
+          <ProviderProduct>
+            <Route exact path="/customer/products" component={ CostumerProducts } />
+            <Route exact path="/customer/checkout" component={ CustomerCheckout } />
+          </ProviderProduct>
+          <Route exact path="/admin/manage" component={ AdminManage } />
+        </ProviderAuth>
       </Switch>
     </BrowserRouter>
   );
