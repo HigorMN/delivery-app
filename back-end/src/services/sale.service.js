46ebd68d5e93fed924c79ab0c0b497df7c66dbe9
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-04-04 19:51:46 -0300

    feat: insert bulk

--- back-end/src/services/sale.service.js
+++ back-end/src/services/sale.service.js
@@ -1,8 +1,15 @@
-const { Sale } = require('../database/models');
+const { Sale, SaleProduct } = require('../database/models');
 const { response } = require('../utils/response');
 
 const create = async (body) => {
+    const { product } = body;
+    
     const sale = await Sale.create(body);
+
+    const insert = product.map(({ id, quantity }) => 
+        ({ saleId: sale.id, productId: id, quantity }));
+
+    await SaleProduct.bulkCreate(insert);
 
     return response(201, sale);
 };
